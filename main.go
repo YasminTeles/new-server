2e20f19b2e21f816b4fc6f696002f878f9cd4850
Yasmin Teles <yasmin.gomes@faci.ly>
2021-09-15 16:07:13 -0300

    feat: add simple server

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/urfave/negroni"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "Welcome to the home page!")
+	})
+
+	n := negroni.Classic()
+	n.UseHandler(mux)
+
+	http.ListenAndServe(":3000", n)
 }
