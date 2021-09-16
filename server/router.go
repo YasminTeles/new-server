8c5be74ae9a8b7c8300f0c4683ea80cb127b9521
Yasmin Teles <yasmin.gomes@faci.ly>
2021-09-16 17:49:57 -0300

    feat: add version route

--- server/router.go
+++ server/router.go
@@ -5,11 +5,17 @@ import (
 	"net/http"
 )
 
+var GitCommit string
+
 func Routers() *http.ServeMux {
 	routers := http.NewServeMux()
 
 	routers.HandleFunc("/healthcheck", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, "Working!")
+	})
+
+	routers.HandleFunc("/version", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, GitCommit)
 	})
 
 	return routers
