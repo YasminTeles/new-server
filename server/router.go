c4b61bf9c556c01a6b4f56cb2aabadd556b6c987
Yasmin Teles <yasmin.gomes@faci.ly>
2021-09-16 15:09:01 -0300

    feat: remove home route

--- server/router.go
+++ server/router.go
@@ -8,10 +8,6 @@ import (
 func Routers() *http.ServeMux {
 	routers := http.NewServeMux()
 
-	routers.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "Welcome to the home page!")
-	})
-
 	routers.HandleFunc("/healthcheck", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, "Working!")
 	})
--- server/router_test.go
+++ server/router_test.go
@@ -8,30 +8,6 @@ import (
 	"testing"
 )
 
-func Test_EndToEnd_Home(t *testing.T) {
-	port := 3000
-
-	request, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("http://localhost:%d/", port), nil)
-
-	client := http.Client{}
-	response, _ := client.Do(request)
-
-	if http.StatusOK != response.StatusCode {
-		t.Errorf("status code got %q, want %q", response.StatusCode, http.StatusOK)
-	}
-
-	byteBody, _ := ioutil.ReadAll(response.Body)
-
-	want := "Welcome to the home page!"
-	got := strings.Trim(string(byteBody), "\n")
-
-	if want != got {
-		t.Errorf("body got %q, want %q", got, want)
-	}
-
-	response.Body.Close()
-}
-
 func Test_EndToEnd_Healthcheck(t *testing.T) {
 	port := 3000
 
