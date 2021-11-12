c2a2f66859175f27fcdc422aaf1a1eb61a5bb83d
Yasmin Teles <teles.yasmin@gmail.com>
2021-11-12 15:22:33 -0300

    tests: refactor endpoint tests

--- server/router_test.go
+++ server/router_test.go
@@ -1,4 +1,4 @@
-//nolint:exhaustivestruct
+//nolint:exhaustivestruct,wsl
 package server_test
 
 import (
@@ -8,26 +8,45 @@ import (
 	"net/http"
 	"strings"
 	"testing"
+	"time"
 
+	"github.com/YasminTeles/new-server/server"
+	"github.com/YasminTeles/new-server/settings"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestHealthcheckEndPoint(t *testing.T) {
-	t.Parallel()
+type ServerTestSuite struct {
+	suite.Suite
+}
 
-	port := 4000
-	url := fmt.Sprintf("http://localhost:%d/healthcheck", port)
+func (suite *ServerTestSuite) SetupSuite() {
+	settings.LoadSettings()
+	go server.ListenAndServe()
+
+	time.Sleep(1 * time.Second)
+}
+
+func (suite *ServerTestSuite) TestHealthcheckEndPoint() {
+	url := fmt.Sprintf("http://localhost:%s/healthcheck", settings.Config.Port)
+
 	request, _ := http.NewRequestWithContext(context.Background(), http.MethodGet, url, nil)
 
 	client := http.Client{}
 	response, _ := client.Do(request)
 
-	assert.Equal(t, http.StatusOK, response.StatusCode)
+	assert.Equal(suite.T(), http.StatusOK, response.StatusCode)
 
 	byteBody, _ := io.ReadAll(response.Body)
 	defer response.Body.Close()
 
 	message := strings.Trim(string(byteBody), "\n")
 
-	assert.Equal(t, "Working!", message)
+	assert.Equal(suite.T(), "Working!", message)
+}
+
+func TestClientTestSuite(t *testing.T) {
+	t.Parallel()
+
+	suite.Run(t, new(ServerTestSuite))
 }
